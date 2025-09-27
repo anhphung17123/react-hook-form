ebf0ac7dff1c580a769781f40968145e96c9f488
anhphung17123 <anhhna17710110@gmail.com>
2025-09-27 22:11:25 +0700

    RHFCheckbox

---
+++ src/components/RHFCheckbox.tsx
@@ -0,0 +1,63 @@
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  FormLabel,
+} from "@mui/material";
+import {
+  Controller,
+  useFormContext,
+  type FieldValues,
+  type Path,
+} from "react-hook-form";
+import type { Option } from "../types/options";
+
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  options?: Option[];
+  label: string;
+};
+
+export function RHFCheckbox<T extends FieldValues>({
+  name,
+  options = [],
+  label,
+}: Props<T>) {
+  const { control } = useFormContext<T>();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { value, onChange }, fieldState: { error } }) => (
+        <FormControl error={!!error}>
+          <FormLabel>{label}</FormLabel>
+          <FormGroup>
+            {options.map((option) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    key={option.id}
+                    checked={value.includes(option.id)}
+                    onChange={() => {
+                      if (value.includes(option.id)) {
+                        onChange(
+                          (value as string[]).filter((id) => id !== option.id)
+                        );
+                      } else {
+                        onChange([...value, option.id]);
+                      }
+                    }}
+                  />
+                }
+                label={option.label}
+              />
+            ))}
+          </FormGroup>
+          <FormHelperText>{error?.message}</FormHelperText>
+        </FormControl>
+      )}
+    />
+  );
+}
