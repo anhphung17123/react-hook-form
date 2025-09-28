e58838d7b03af8d2608e0937333f5f3e58f2019e
anhphung17123 <anhhna17710110@gmail.com>
2025-09-28 14:50:31 +0700

    RHFSlider

---
+++ src/components/RHFSlider.tsx
@@ -0,0 +1,31 @@
+import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import {
+  Controller,
+  useFormContext,
+  type FieldValues,
+  type Path,
+} from "react-hook-form";
+import { Slider, Typography } from "@mui/material";
+
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  label: string;
+};
+
+export function RHFSlider<T extends FieldValues>({ name, label }: Props<T>) {
+  const { control } = useFormContext<T>();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <>
+          <Typography>{label}</Typography>
+          <Slider {...field} valueLabelDisplay="auto" />
+        </>
+      )}
+    />
+  );
+}
